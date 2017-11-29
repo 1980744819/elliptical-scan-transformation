9c249b7c492d28df591b24c63b8e4ae7f61ae898
nankeyimeng <1980744819@qq.com>
2017-11-30 00:54:00 +0800

    新传上去的

---
+++ builds/main.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import math
+
+sz=(1080,720)
+wt=(255,255,255)
+blk=(0,0,0)
+def create_img():
+    im = Image.new("RGB", size=sz, color=wt)
+    # im.show()
+    return im
+
+
+if __name__=="__main__":
+    im=create_img()
+    (a,b)=map(int,input("please input a and b: ").split(' '))
+    print(a,b)
+    x=0
+    y=b
+    d1=b*b+a*a*(0.25-b)
+    while (b*b*(x+1))<(a*a*(y-0.5)):
+        if d1<0:
+            d1+=b*b*(2*x+3)
+            x+=1
+        else:
+            d1+=(b*b*(2*x+3))+a*a*(-2*y+2)
+            x+=1
+            y-=1
+        im.putpixel((x,y),blk)
+    d2=math.sqrt(b*(x+0.5))+math.sqrt(a*(y-1))-math.sqrt(a*b)
+    while y>0:
+        if d2<0:
+            d2+=b*b*(2*x+2)+a*a*(y*(-2)+3)
+            x+=1
+            y-=1
+        else:
+            d2+=a*a*(-2*y+3)
+            y-=1
+        im.putpixel((x,y),blk)
+    im.show()
